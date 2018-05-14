df3290fc743c7f6d40286c15810fb261e734398f
Jacob Kattampilly <jacobkuriala@gmail.com>
2018-05-13 18:33:20 -0700

    Removed irrelevant dashboard routes

--- src/routes/dashboard.jsx
+++ src/routes/dashboard.jsx
@@ -64,145 +64,33 @@ var dashRoutes = [
     icon: DashboardIcon,
     component: Dashboard
   },
-  {
-    collapse: true,
-    path: "-page",
-    name: "Pages",
-    state: "openPages",
-    icon: Image,
-    views: pages
-  },
-  {
-    collapse: true,
-    path: "/components",
-    name: "Components",
-    state: "openComponents",
-    icon: Apps,
-    views: [
-      {
-        path: "/components/buttons",
-        name: "Buttons",
-        mini: "B",
-        component: Buttons
-      },
-      {
-        path: "/components/grid-system",
-        name: "Grid System",
-        mini: "GS",
-        component: GridSystem
-      },
-      {
-        path: "/components/panels",
-        name: "Panels",
-        mini: "P",
-        component: Panels
-      },
-      {
-        path: "/components/sweet-alert",
-        name: "Sweet Alert",
-        mini: "SA",
-        component: SweetAlert
-      },
-      {
-        path: "/components/notifications",
-        name: "Notifications",
-        mini: "N",
-        component: Notifications
-      },
-      { path: "/components/icons", name: "Icons", mini: "I", component: Icons },
-      {
-        path: "/components/typography",
-        name: "Typography",
-        mini: "T",
-        component: Typography
-      }
-    ]
-  },
-  {
-    collapse: true,
-    path: "/forms",
-    name: "Forms",
-    state: "openForms",
-    icon: ContentPaste,
-    views: [
-      {
-        path: "/forms/regular-forms",
-        name: "Regular Forms",
-        mini: "RF",
-        component: RegularForms
-      },
-      {
-        path: "/forms/extended-forms",
-        name: "Extended Forms",
-        mini: "EF",
-        component: ExtendedForms
-      },
-      {
-        path: "/forms/validation-forms",
-        name: "Validation Forms",
-        mini: "VF",
-        component: ValidationForms
-      },
-      { path: "/forms/wizard", name: "Wizard", mini: "W", component: Wizard }
-    ]
-  },
-  {
-    collapse: true,
-    path: "/tables",
-    name: "Tables",
-    state: "openTables",
-    icon: GridOn,
-    views: [
-      {
-        path: "/tables/regular-tables",
-        name: "Regular Tables",
-        mini: "RT",
-        component: RegularTables
-      },
-      {
-        path: "/tables/extended-tables",
-        name: "Extended Tables",
-        mini: "ET",
-        component: ExtendedTables
-      },
-      {
-        path: "/tables/react-tables",
-        name: "React Tables",
-        mini: "RT",
-        component: ReactTables
-      }
-    ]
-  },
-  {
-    collapse: true,
-    path: "/maps",
-    name: "Maps",
-    state: "openMaps",
-    icon: Place,
-    views: [
-      {
-        path: "/maps/google-maps",
-        name: "Google Maps",
-        mini: "GM",
-        component: GoogleMaps
-      },
-      {
-        path: "/maps/full-screen-maps",
-        name: "Full Screen Map",
-        mini: "FSM",
-        component: FullScreenMap
-      },
-      {
-        path: "/maps/vector-maps",
-        name: "Vector Map",
-        mini: "VM",
-        component: VectorMap
-      }
-    ]
-  },
-  { path: "/widgets", name: "Widgets", icon: WidgetsIcon, component: Widgets },
-  { path: "/charts", name: "Charts", icon: Timeline, component: Charts },
-  { path: "/calendar", name: "Calendar", icon: DateRange, component: Calendar },
+    {
+        collapse: true,
+        path: "/tables",
+        name: "Tables",
+        state: "openTables",
+        icon: GridOn,
+        views: [
+            {
+                path: "/tables/regular-tables",
+                name: "Regular Tables",
+                mini: "RT",
+                component: RegularTables
+            },
+            {
+                path: "/tables/extended-tables",
+                name: "Extended Tables",
+                mini: "ET",
+                component: ExtendedTables
+            },
+            {
+                path: "/tables/react-tables",
+                name: "React Tables",
+                mini: "RT",
+                component: ReactTables
+            }
+        ]
+    },
   { redirect: true, path: "/", pathTo: "/dashboard", name: "Dashboard" }
 ];
 export default dashRoutes;
